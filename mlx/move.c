9230b14ec093edc41c0fd24ef27d8563b8fe9071
Mehdi Boughrara <mboughra@e3r4p12.1337.ma>
2024-03-04 18:24:32 +0100

    collectibles no longer gets printed in terminal

--- mlx/move.c
+++ mlx/move.c
@@ -6,7 +6,7 @@
 /*   By: mboughra <mboughra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 18:08:57 by mboughra          #+#    #+#             */
-/*   Updated: 2024/03/04 17:47:17 by mboughra         ###   ########.fr       */
+/*   Updated: 2024/03/04 18:24:04 by mboughra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ t_struct	*move_right(t_struct *d)
 		d->map2[d->px][d->py] = '0';
 		mlx_put_image_to_window(d->mlx, d->w, d->flr, d->py * 64, d->px * 64);
 		d->co_count--;
-		ft_printf("collectibles -> %d\n", d->co_count);
 	}
 	mlx_put_image_to_window(d->mlx, d->w, d->player, d->py * 64, d->px * 64);
 	mlx_put_image_to_window(d->mlx, d->w, d->flr, (d->py - 1) * 64, d->px * 64);
@@ -64,7 +63,6 @@ t_struct	*move_left(t_struct *d)
 		d->map2[d->px][d->py] = '0';
 		mlx_put_image_to_window(d->mlx, d->w, d->flr, d->py * 64, d->px * 64);
 		d->co_count--;
-		ft_printf("collectibles -> %d\n", d->co_count);
 	}
 	mlx_put_image_to_window(d->mlx, d->w, d->player, d->py * 64, d->px * 64);
 	mlx_put_image_to_window(d->mlx, d->w, d->flr, d->py * 64, (d->px - 1) * 64);
@@ -90,7 +88,6 @@ t_struct	*move_down(t_struct *d)
 		d->map2[d->px][d->py] = '0';
 		mlx_put_image_to_window(d->mlx, d->w, d->flr, d->py * 64, d->px * 64);
 		d->co_count--;
-		ft_printf("collectibles -> %d\n", d->co_count);
 	}
 	mlx_put_image_to_window(d->mlx, d->w, d->player, d->py * 64, d->px * 64);
 	mlx_put_image_to_window(d->mlx, d->w, d->flr, (d->py + 1) * 64, d->px * 64);
@@ -116,7 +113,6 @@ t_struct	*move_up(t_struct *d)
 		d->map2[d->px][d->py] = '0';
 		mlx_put_image_to_window(d->mlx, d->w, d->flr, d->py * 64, d->px * 64);
 		d->co_count--;
-		ft_printf("collectibles -> %d\n", d->co_count);
 	}
 	mlx_put_image_to_window(d->mlx, d->w, d->player, d->py * 64, d->px * 64);
 	mlx_put_image_to_window(d->mlx, d->w, d->flr, d->py * 64, (d->px + 1) * 64);
